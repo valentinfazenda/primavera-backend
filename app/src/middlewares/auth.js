c5b697552c5209419806bf283f3cfd79a4a8e8f5
Fazenda <valentin.fazenda@sinequa.com>
2024-05-24 11:28:18 +0200

    get and create flows

--- app/src/middlewares/auth.js
+++ app/src/middlewares/auth.js
@@ -1,16 +1,23 @@
 const jwt = require('jsonwebtoken');
 
 const authenticateToken = (req, res, next) => {
-    const token = req.header('Authorization').replace('Bearer ', '');
-  if (!token) return res.status(401).json({ msg: 'No token, authorization denied' });
+    const authHeader = req.header('Authorization');
+    if (!authHeader) {
+        return res.status(401).json({ msg: 'No token, authorization denied' });
+    }
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded.user;
-    next();
-  } catch (err) {
-    res.status(401).json({ msg: 'Token is not valid' });
-  }
+    const token = authHeader.replace('Bearer ', '');
+    if (!token) {
+        return res.status(401).json({ msg: 'No token, authorization denied' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded.user;
+        next();
+    } catch (err) {
+        res.status(401).json({ msg: 'Token is not valid' });
+    }
 };
 
 module.exports = authenticateToken;
--- app/src/routes/flows/flows.js
+++ app/src/routes/flows/flows.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authenticateToken = require('../../middlewares/auth');
 const Flow = require('../../models/Flow/Flow');
 
-router.get('/flows', authenticateToken, async (req, res) => {
+router.get('', authenticateToken, async (req, res) => {
   try {
     const userId = req.user.id;
     
@@ -16,4 +16,27 @@ router.get('/flows', authenticateToken, async (req, res) => {
   }
 });
 
+router.post('/create', authenticateToken, async (req, res) => {
+    try {
+        const userId = req.user.id;
+        const { data } = req.body;
+
+        if (!data) {
+        return res.status(400).json({ error: "Data is required" });
+        }
+  
+        const newFlow = new Flow({
+        userid: userId,
+        data: data
+        });
+
+        const savedFlow = await newFlow.save();
+
+        res.status(201).json(savedFlow);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error.message });
+    }
+    });
+  
 module.exports = router;
