8800e306df6167c9f76f9a1862efaa879977928f
Fazenda <valentin.fazenda@sinequa.com>
2024-05-27 16:02:28 +0200

    add user/details endpoint

--- app/src/routes/user/user.js
+++ app/src/routes/user/user.js
@@ -27,4 +27,20 @@ router.post('/update', authenticateToken, async (req, res) => {
     }
 });
 
+router.get('/details', authenticateToken, async (req, res) => {
+    try {
+        const userId = req.user.id;
+
+        const user = await User.findById(userId).select('-password');
+        if (!user) {
+            return res.status(404).json({ error: "User not found." });
+        }
+
+        res.status(200).json(user);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server error" });
+    }
+});
+
 module.exports = router;
