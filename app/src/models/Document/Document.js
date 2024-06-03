9a686ff273fa9d8eeb6dec8788493943eafb3538
Fazenda <valentin.fazenda@sinequa.com>
2024-06-03 18:30:26 +0200

    add document model

---
+++ app/src/models/Document/Document.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const DocumentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  fulltext: {
+    type: String,
+    required: true
+  },
+  extension: {
+    type: String,
+    required: true,
+    lowercase: true,
+    trim: true
+  }
+});
+
+DocumentSchema.index({ fulltext: 'text' });
+
+const Document = mongoose.model('Document', DocumentSchema);
+
+module.exports = Document;
