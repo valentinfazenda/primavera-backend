ac82af28396cb138032a709dad90bffd1bae49de
Fazenda <valentin.fazenda@sinequa.com>
2024-06-03 13:13:46 +0200

    optimize stepllmservice

--- app/src/services/run/stepService/stepLlmService/stepLlmService.js
+++ app/src/services/run/stepService/stepLlmService/stepLlmService.js
@@ -3,37 +3,25 @@ const Step = require('../../../../models/Step/Step');
 const { sendMessageToAzureOpenAI } = require('../../../llms/azureOpenAIService/azureOpenAIService');
 const { sendMessageToOpenAI } = require('../../../llms/openAIService/openAIService');
 
-async function executeStepLlm(stepId, userId, input = '') {
+async function executeStepLlm(stepId, userId, input = '', socket = null) {
   try {
-    const step = await Step.findById(stepId);
-    if (!step) {
-      throw new Error("Step not found");
-    }
-    const messages = [];
-    if (input != '') {
-        messages.push({
-            role: "user",
-            content: input + "\n\n\nConsidering the above input the user wants to perform this task " + step.data + " Answer:"
-        });
-    } else {
-        messages.push({
-            role: "user",
-            content: input + "\n\n\n" + step.data
-        });
-    }
-    if (step.modelLlm = "AzureOpenAI") {
-        return await sendMessageToAzureOpenAI(userId, messages, step.modelName);
-    }
-    else if (step.modelLlm = "OpenAI") {
-        return await sendMessageToOpenAI(userId, messages, step.modelName);
-    }
-    else {
-        throw new Error("Model not found");
-    }
+    const step = await Step.findById(stepId).orFail(new Error("Step not found"));
+    const messages = [{
+        role: "user",
+        content: `${input}\n\n\n${input ? `Considering the above input the user wants to perform this task ${step.data} Answer:` : step.data}`
+    }];
 
+    switch (step.modelLlm) {
+        case "AzureOpenAI":
+            return await sendMessageToAzureOpenAI(userId, messages, step.modelName, socket);
+        case "OpenAI":
+            return await sendMessageToOpenAI(userId, messages, step.modelName, socket);
+        default:
+            throw new Error("Model not found");
+    }
   } catch (error) {
     console.error(error);
-    throw new Error(error.message);
+    throw error;
   }
 }
 
