0d8026893d2545812c4450a7c261604482843c54
Fazenda <valentin.fazenda@sinequa.com>
2024-06-06 18:45:42 +0200

    adjust socket.js

--- socket.js
+++ socket.js
@@ -1,13 +1,11 @@
 import executeFlow from './app/src/services/run/flowService/flowService.js';
 
-// Définir la fonction
 function handleConnection(socket) {
     socket.on('message', async (msg) => {
         try {
             socket.emit('authenticated', { status: 'success' });
             if (msg.flowId) {
                 const messageContent = await executeFlow(msg.flowId, socket.user.id, socket);
-                socket.emit('message', messageContent);
             } else {
                 console.error('missing flowId');
                 socket.emit('message', { status: 'error', message: "missing flowId" });
