66bdbe7b725962cdb2effd851e666f554e67530e
Fazenda <valentin.fazenda@sinequa.com>
2024-10-08 18:56:30 +0200

    Update User model with status field and rename date field to creationDate

--- app/src/models/User/User.js
+++ app/src/models/User/User.js
@@ -4,10 +4,11 @@ const UserSchema = new mongoose.Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    status : { type: String, required: true, default:'user', enum: ['user', 'admin'] },
     password: { type: String, required: true },
     profilePicture: { type: String, required: false },
     companyId: { type: String, required: false },
-    date: { type: Date, default: Date.now }
+    creationDate: { type: Date, default: Date.now }
 });
 
 const User = mongoose.model('User', UserSchema);
