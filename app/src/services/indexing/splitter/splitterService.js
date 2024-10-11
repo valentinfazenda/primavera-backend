1d21dc246fc8f368688c8c846e5bddc2eabd5851
Fazenda <valentin.fazenda@sinequa.com>
2024-10-11 18:05:49 +0200

    Refactor Document model and add status field

--- app/src/services/indexing/splitter/splitterService.js
+++ app/src/services/indexing/splitter/splitterService.js
@@ -1,4 +1,3 @@
-import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 import Document from '../../../models/Document/Document.js';
 
 // Function to process the document using content from a local file
@@ -27,21 +26,6 @@ async function splitDocumentToChunks(documentId) {
         console.error('Error in processDocument:', error);
         throw error;
     }
-}
-
-// Function to split text into chunks using Langchain's RecursiveCharacterTextSplitter
-async function splitTextIntoChunks(text) { // Mark function as async if the splitting is asynchronous
-    // Use Langchain's RecursiveCharacterTextSplitter
-    console.log('Splitting text into chunks...');
-    const splitter = new RecursiveCharacterTextSplitter({
-        chunkSize: 6000,  // Maximum number of characters per chunk
-        chunkOverlap: 200  // Number of characters overlapping between chunks
-    });
-    console.log('Splitter:', splitter);
-
-    const chunks = splitter.splitText(text); // Await this call if it's asynchronous
-    console.log('Chunks:', chunks);
-    return chunks;  // Returns an array of chunks
 }
 
 // Function to split text into sentences
